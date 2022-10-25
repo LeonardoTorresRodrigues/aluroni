c89a5a3b021e201fc7c7b1a9b452b90147b87ff8
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-10-25 10:43:49 -0300

    [ADD]ablosute import [ADD]reset.css

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,5 +1,5 @@
 import styles from './Cardapio.module.scss';
-import {ReactComponent as Logo} from '../../assets/logo.svg';
+import {ReactComponent as Logo} from 'assets/logo.svg';
 
 export default function Cardapio() {
   return (
