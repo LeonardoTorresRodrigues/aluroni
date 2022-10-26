7abd6cddc63e631f1cd059e6272cf8ee224c55c4
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-10-26 10:52:54 -0300

    [CREATE] Filter for the app

---
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -0,0 +1,21 @@
+import filtros from './filtros.json';
+import styles from './Filtros.module.scss';
+
+type IOpcao = typeof filtros[0];
+
+export default function Filtros() {
+
+  function selecionarFiltro(opcao: IOpcao) {
+
+  }
+
+  return (
+    <div className={styles.filtros}>
+      {filtros.map((opcao) => (
+        <button className={styles.filtros__filtro} key={opcao.id} onClick={() => selecionarFiltro(opcao)}>
+          {opcao.label}
+        </button>
+      ))}
+    </div>
+  )
+}
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,8 @@
 import styles from './Cardapio.module.scss';
-import {ReactComponent as Logo} from 'assets/logo.svg';
+import { ReactComponent as Logo } from 'assets/logo.svg';
 import Buscador from './Buscador';
 import { useState } from 'react';
+import Filtros from './Filtros';
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
@@ -18,8 +19,11 @@ export default function Cardapio() {
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
         <Buscador
-        busca={busca}
-        setBusca={setBusca}/>
+          busca={busca}
+          setBusca={setBusca} />
+        <div className={styles.cardapio__filtros}>
+          <Filtros />
+        </div>
       </section>
     </main>
   )
