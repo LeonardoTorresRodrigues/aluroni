64cce10ce2377648c7bf828d2dcd31db74704dd6
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-10-27 12:24:19 -0300

    [FIX] style of tags and item

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -25,16 +25,15 @@ export default function Item(props: Props) {
           >
             {category.label}
           </div>
-          {category.label}
-        </div>
-        <div className={styles.item__porcao}>
-          {size}g
-        </div>
-        <div className={styles.item__qtdpessoas}>
-          Serve {serving} pessoa{serving === 1 ? "" : "s"}
-        </div>
-        <div className={styles.item__valor}>
-          R$ {price.toFixed(2)}
+          <div className={styles.item__porcao}>
+            {size}g
+          </div>
+          <div className={styles.item__qtdpessoas}>
+            Serve {serving} pessoa{serving === 1 ? "" : "s"}
+          </div>
+          <div className={styles.item__valor}>
+            R$ {price.toFixed(2)}
+          </div>
         </div>
       </div>
     </div>
