eb14ba415611e18d36317a9a349708867852ca57
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-10-27 11:53:36 -0300

    [UPDATE] clasNames of types

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -18,22 +18,23 @@ export default function Item(props: Props) {
         </div>
         <div className={styles.item__tags}>
           <div
-            className={classNames({
-              [styles.item__tipo]: true,
-              [styles[`item__tipo__${category.label.toLowerCase()}`]]: true,
-            })}
+            className={classNames(
+              styles.item__tipo,
+              styles[`item__tipo__${category.label.toLowerCase()}`]
+            )}
           >
             {category.label}
           </div>
-          <div className={styles.item__porcao}>
-            {size}g
-          </div>
-          <div className={styles.item__qtdpessoas}>
-            Serve {serving} pessoa{serving === 1 ? "" : "s"}
-          </div>
-          <div className={styles.item__valor}>
-            R$ {price.toFixed(2)}
-          </div>
+          {category.label}
+        </div>
+        <div className={styles.item__porcao}>
+          {size}g
+        </div>
+        <div className={styles.item__qtdpessoas}>
+          Serve {serving} pessoa{serving === 1 ? "" : "s"}
+        </div>
+        <div className={styles.item__valor}>
+          R$ {price.toFixed(2)}
         </div>
       </div>
     </div>
