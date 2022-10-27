99dfb12b1674df821c722265e3b9e3c53f4301bf
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-10-27 11:05:14 -0300

    [CREATE] Itens

---
+++ src/pages/Cardapio/Itens/index.tsx
@@ -0,0 +1,13 @@
+import cardapio from './itens.json';
+import Item from './Item';
+import styles from './Itens.module.scss';
+
+export default function Itens() {
+  return (
+    <div className={styles.itens}>
+      {cardapio.map((item) => (
+        <Item key={item.id} />
+      ))}
+    </div>
+  );
+}
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -4,6 +4,7 @@ import Buscador from './Buscador';
 import { useState } from 'react';
 import Filtros from './Filtros';
 import Ordenador from './Ordenador';
+import Itens from './Itens';
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
@@ -28,6 +29,7 @@ export default function Cardapio() {
           <Filtros filtro={filtro} setFiltro={setFiltro} />
           <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
         </div>
+        <Itens />
       </section>
     </main>
   )
