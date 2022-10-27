c371421544f4c6b069e6b1dc5242de23427b63a6
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-10-27 11:19:51 -0300

    [ADD] Itens from cardapio

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -6,7 +6,10 @@ export default function Itens() {
   return (
     <div className={styles.itens}>
       {cardapio.map((item) => (
-        <Item key={item.id} />
+        <Item
+          key={item.id}
+          {...item}
+        />
       ))}
     </div>
   );
