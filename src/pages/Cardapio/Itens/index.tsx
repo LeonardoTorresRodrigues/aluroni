d7b813b00a631cc69b38bf09df8feb4a233cbb88
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-10-28 11:13:40 -0300

    [WORKING] filters

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,11 +1,36 @@
 import cardapio from './itens.json';
 import Item from './Item';
 import styles from './Itens.module.scss';
+import { useEffect, useState } from 'react';
 
-export default function Itens() {
+interface Props {
+  busca: string;
+  filtro: number | null;
+  ordenador: string;
+}
+
+export default function Itens(props: Props) {
+  const [lista, setLista] = useState(cardapio);
+  const { busca, filtro } = props;
+
+  function testaBusca(title: string) {
+    const regex = new RegExp(busca, 'i');
+    return regex.test(title);
+  }
+
+  function testaFiltro(id: number) {
+    if(filtro !== null) return filtro === id;
+    return true;
+  }
+
+  useEffect(() => {
+    const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
+    setLista(novaLista);
+  }, [busca, filtro]);
+
   return (
     <div className={styles.itens}>
-      {cardapio.map((item) => (
+      {lista.map((item) => (
         <Item
           key={item.id}
           {...item}
