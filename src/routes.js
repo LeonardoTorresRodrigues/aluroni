23dc135b391e8603efaff4d88ad515b5a016844c
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-01-25 11:57:13 -0300

    refactor: menu style via routes

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import Menu from 'components/Menu';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -5,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 export default function AppRouter() {
   return (
     <Router>
+      <Menu /> {/**passando o estilo de menu diretamente pela rota */}
       <Routes>
         <Route path='/' element={<Inicio />} />
         <Route path='/cardapio' element={<Cardapio />} />
