fea2f9c8c8a83110ae4502461daced7de06c333c
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-01-25 11:55:46 -0300

    refactor: remove menu style

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,5 +1,4 @@
 import styles from './Cardapio.module.scss';
-import { ReactComponent as Logo } from 'assets/logo.svg';
 import Buscador from './Buscador';
 import { useState } from 'react';
 import Filtros from './Filtros';
@@ -12,9 +11,6 @@ export default function Cardapio() {
   const [ordenador, setOrdenador] = useState<OpcoesOrdenador>('');
   return (
     <main>
-      <nav className={styles.menu}>
-        <Logo />
-      </nav>
       <header className={styles.header}>
         <div className={styles.header__text}>
           A casa do código e da massa
