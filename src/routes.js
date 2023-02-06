81aaaa351c289802d621c1ba0a921177c2c23377
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-02-06 13:49:49 -0300

    feat: new route element "PaginaPadrao"

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,21 @@
 import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Menu /> {/**passando o estilo de menu diretamente pela rota */}
-      <Routes>
-        <Route path='/' element={<Inicio />} />
-        <Route path='/cardapio' element={<Cardapio />} />
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Menu /> {/**passando o estilo de menu diretamente pela rota */}
+        <Routes>
+          <Route path='/' element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+          </Route>
+        </Routes>
+      </Router>
+    </main>
   );
 }
