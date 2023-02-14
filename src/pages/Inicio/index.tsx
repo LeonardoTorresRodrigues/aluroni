c0b40c23fd948836b839a4a54350aac68be670a2
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-02-14 16:50:46 -0300

    feat: add address on home (Início) page

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,6 +1,7 @@
 import cardapio from '../../data/cardapio.json';
 import styles from './Inicio.module.scss';
 import stylesTema from 'styles/Tema.module.scss';
+import nossaCasa from 'assets/nossa_casa.png';
 
 export default function Inicio() {
   let pratosRecomendados = [...cardapio];
@@ -21,6 +22,13 @@ export default function Inicio() {
             </button>
           </div>
         ))}
+      </div>
+      <h3 className={stylesTema.titulo}> Nossa casa </h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt="Casa do aluroni" />
+        <div className={styles.nossaCasa__endereco}>
+          Rua Vergueiro, 3185 <br /> <br /> Vila  Mariana - SP
+        </div>
       </div>
     </section>
   );
