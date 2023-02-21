1431d634a04790466d2114a9bb9f54853b392c86
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-02-21 16:40:29 -0300

    feat: new function redirecting to dishes details page

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -2,10 +2,17 @@ import cardapio from '../../data/cardapio.json';
 import styles from './Inicio.module.scss';
 import stylesTema from 'styles/Tema.module.scss';
 import nossaCasa from 'assets/nossa_casa.png';
+import { useNavigate } from 'react-router-dom';
 
 export default function Inicio() {
   let pratosRecomendados = [...cardapio];
   pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0, 3);
+  const navigate = useNavigate();
+
+  function redirecionarParaDetalhes(prato: typeof cardapio[0]) {
+    navigate(`/prato/${prato.id}`, { state: { prato }, replace: true });
+  }
+
   return (
     <section>
       <h3 className={stylesTema.titulo}>
@@ -17,7 +24,10 @@ export default function Inicio() {
             <div className={styles.recomendado__imagem}>
               <img src={item.photo} alt={item.title} />
             </div>
-            <button className={styles.recomendado__botao}>
+            <button
+              className={styles.recomendado__botao}
+              onClick={() => redirecionarParaDetalhes(item)}
+            >
               Ver mais
             </button>
           </div>
