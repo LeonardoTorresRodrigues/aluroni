4204ef608fc89c6f3faf254eba7a14db7f740711
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-02-21 17:48:52 -0300

    refactor: using TagsPrato component

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,10 +1,10 @@
 import styles from './Item.module.scss';
-import classNames from 'classnames';
 import { Prato } from 'types/Prato';
+import TagsPrato from 'components/TagsPrato';
 
 
 export default function Item(props: Prato) {
-  const { title, description, category, size, serving, price, photo } = props;
+  const { title, description, photo } = props;
   return (
     <div className={styles.item}>
       <div className={styles.item__imagem}>
@@ -15,25 +15,7 @@ export default function Item(props: Prato) {
           <h2> {title} </h2>
           <p> {description} </p>
         </div>
-        <div className={styles.item__tags}>
-          <div
-            className={classNames(
-              styles.item__tipo,
-              styles[`item__tipo__${category.label.toLowerCase()}`]
-            )}
-          >
-            {category.label}
-          </div>
-          <div className={styles.item__porcao}>
-            {size}g
-          </div>
-          <div className={styles.item__qtdpessoas}>
-            Serve {serving} pessoa{serving === 1 ? '' : 's'}
-          </div>
-          <div className={styles.item__valor}>
-            R$ {price.toFixed(2)}
-          </div>
-        </div>
+        <TagsPrato {...props} />
       </div>
     </div>
   );
